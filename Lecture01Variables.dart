136d426bea839ae4c97de6583b2f545e4f21bd20
maaz <maazsaleem2265@gmail.com>
2025-03-30 06:40:20 -0700

    Lecture01Variables

---
+++ Lecture01Variables.dart
@@ -0,0 +1,23 @@
+void main(){
+ print("Variables \n"); //printing five pillers of islam variables using var data types
+
+var Shahada = "There is no god but Allah, and Muhammad is His Messenger.";  //var data type is use when we have to print some text using var data type
+var salah = "Performing five daily prayers at specific times.";  //var data type is use when we have to print some text using var data type
+var Zakat = "Giving a portion of one's wealth to those in need (usually 2.5% of savings)"; //var data type is use when we have to print some text using var data type
+var Sawm = "Fasting during the month of Ramadan from dawn to sunset."; //var data type is use when we have to print some text using var data type
+var Hajj = " A pilgrimage to Mecca that every Muslim must try to perform at least once in their lifetime if they are physically and financially able."; //var data type is use when we have to print some text using var data type
+
+
+print("$Shahada \n $salah \n$Zakat \n$Sawm \n$Hajj"); // Here i printed the variables in print function.
+
+print("Data Types \n"); //now here we see some data types
+
+var Name="Maaz";  //var data type is use when we have to print some text using var data type
+int age = 19; //integer data type is used to print integers numbers only e.g 9,19 etc
+num height= 5.4; //num data type can handle both float and double data type so if we have to pring any decimal value then use num data type
+bool isbropraynamazfivetimesdaliy = true; //bool data type is use to ring true or false lik for telling user if the statement is true or false
+
+
+print("Name: $Name \n Age: $age \n Height: $height \n Question: $isbropraynamazfivetimesdaliy"); //here i printed the variables
+
+}
---
+++ Lecture02Arithmetic.dart
@@ -0,0 +1,27 @@
+void main(){
+// nisab 
+print("Nisab");
+num zakat= 2.5;
+num wealth = 100000;
+num total = wealth * zakat / 100;
+
+print("$total \n");
+
+
+// for Sadaqah
+print("Sadaqah");
+int permeal = 500;
+int numberofpeople = 10;
+int amount = permeal * numberofpeople;
+
+print(amount);
+
+
+
+
+
+}
+
+
+
+
